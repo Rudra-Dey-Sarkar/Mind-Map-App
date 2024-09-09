0021d3ad6c6b5417332f94b5bc55df6cd0d37f63
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-09-09 15:02:25 +0530

    Update Datas.js

--- src/components/Body/Datas.js
+++ src/components/Body/Datas.js
@@ -100,5 +100,306 @@ export const Datas = [
             delete: DeleteLogo,
         }
     },
-
+    ///////////////////////////////////////////////////////////////////////////////////////
+    {
+        data: "Who founded beyondchats? The founders of BeyondChats are Pankaj Baranwal and Simran Jain.",
+        source: "--",
+        type: "Text",
+        created: "2/20/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "How do I get started? To get started schedule an onboarding connect with our experts. They will understand your business and help you integrate the chatbot in 2 minutes. Schedule a short 15-minutes connect with one of our experts [here](https://calendly.com/beyondchats/15min) Read More",
+        source: "--",
+        type: "Text",
+        created: "2/17/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Why chose you We are not just a conversational chatbot. Our AI builds trust with users and nudges them towards business actions like buying product, requesting demo, etc.",
+        source: "--",
+        type: "Text",
+        created: "20/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What will happen during the onboarding process? During the onboarding process, our experts will understand your business needs and try to understand how you want your chatbot to respond. Then they will build the chatbot and share the code within a day. [Book Call Now](https://calendly.com/beyondchats/15min)",
+        source: "--",
+        type: "Text",
+        created: "20/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Registration link? To register for the free trial book an onboarding call with our team. The team will share the next steps of the registration in the call. Book Call Now",
+        source: "--",
+        type: "Text",
+        created: "8/6/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What kind of analytics and reporting features does the chatbot offer? aily: Leads stats + audience segmentation based on buying intent of users Weekly & Monthly: Leads stats, Top questions asked by users, Useful Sales Intelligence Analytics",
+        source: "--",
+        type: "Text",
+        created: "9/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "in which company you have provided your chatbot We have provided our chatbot services to various companies across different industries.  These include: MultiBhashi, Selligion, drmalpani. com, indianangel. in May I know which company / industry are you coming from?",
+        source: "--",
+        type: "Text",
+        created: "15/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Can I cancel my subscription at any time?  You may cancel your subscription to BeyondChats at any time.  The cancellation will become effective at the end of your current billing cycle.  Upon cancellation, your service will remain active until the end of your paid billing term.",
+        source: "--",
+        type: "Text",
+        created: "8/6/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What are the pricing plans of BeyondChats? We offer 3 different plans: Individual Plan at Rs 1,000 only, Startup Plan at Rs 8,000 only, and Enterprise Plan price based on your requirements. You can subscribe to the paid plans in a monthly or an annual billing cycle. [Know More](https://beyondchats.com/)",
+        source: "--",
+        type: "Text",
+        created: "20/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Contact Details Please write to contactbeyondchats@gmail.com and we'll get back to you real soon!",
+        source: "--",
+        type: "Text",
+        created: "13/3/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Do you work on whatsapp? Yes, we do offer our services on WhatsApp!",
+        source: "--",
+        type: "Text",
+        created: "3/8/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "I want to test your chatbot That's great to hear! You can continue chatting with me to test BeyondChats AI responses! I am one of the AIs! 😏 Else, you can also book a demo call with my team: https://calendly.com/beyondchats/15min",
+        source: "--",
+        type: "Text",
+        created: "28/3/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Will the Startup Plan address the concern I mentioned earlier? Yes, the Startup Plan will definitely help address your concerns! With this plan, you'll have access to advanced analytics and features that can enhance customer interactions while ensuring data security.",
+        source: "--",
+        type: "Text",
+        created: "31/7/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What is the cost of IVF? I can’t provide specific information on IVF costs, but I can tell you about how our chatbot services can enhance your business by generating high-quality leads and providing 24/7 support to your customers.",
+        source: "--",
+        type: "Text",
+        created: "31/7/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Will the Startup Plan address the concern I mentioned earlier? Yes, the Startup Plan will definitely help address your concerns! With this plan, you'll have access to advanced analytics and features that can enhance customer interactions while ensuring data security.",
+        source: "--",
+        type: "Text",
+        created: "31/7/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Which languages can you talk in? Multilingual support is the core of my AI!  I can speak many popular languages.  Just talk in the language you are comfortable in! Supported languages include: Regional Indian languages like Hindi, Marathi, Bengali, Tamil, etc. International languages I can speak fluently: German, French, Portuguese, etc.",
+        source: "--",
+        type: "Text",
+        created: "13/3/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "who are you My name is Bech! I am an AI chatbot and my job is to help you with any questions you may have about BeyondChats or our services.",
+        source: "--",
+        type: "Text",
+        created: "13/3/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Do you have live chat support? Yes, we do have live chat support, but we primarily focus on providing chatbot solutions that enhance customer interactions and offer instant responses 24/7.",
+        source: "--",
+        type: "Text",
+        created: "31/7/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "I want to freeze my eggs. That's great, please choose from one of these 3 options.",
+        source: "--",
+        type: "Text",
+        created: "14/8/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "I am trying to get pregnant.We have helped thousands of couples over the years, and we would love to support you too. Please choose from one of these three options.",
+        source: "--",
+        type: "Text",
+        created: "14/8/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    ///////////////////////////////////////////////////////////////////////////////////////
+    {
+        data: "Who founded beyondchats? The founders of BeyondChats are Pankaj Baranwal and Simran Jain.",
+        source: "--",
+        type: "Text",
+        created: "2/20/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "How do I get started? To get started schedule an onboarding connect with our experts. They will understand your business and help you integrate the chatbot in 2 minutes. Schedule a short 15-minutes connect with one of our experts [here](https://calendly.com/beyondchats/15min) Read More",
+        source: "--",
+        type: "Text",
+        created: "2/17/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Why chose you We are not just a conversational chatbot. Our AI builds trust with users and nudges them towards business actions like buying product, requesting demo, etc.",
+        source: "--",
+        type: "Text",
+        created: "20/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What will happen during the onboarding process? During the onboarding process, our experts will understand your business needs and try to understand how you want your chatbot to respond. Then they will build the chatbot and share the code within a day. [Book Call Now](https://calendly.com/beyondchats/15min)",
+        source: "--",
+        type: "Text",
+        created: "20/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Registration link? To register for the free trial book an onboarding call with our team. The team will share the next steps of the registration in the call. Book Call Now",
+        source: "--",
+        type: "Text",
+        created: "8/6/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What kind of analytics and reporting features does the chatbot offer? aily: Leads stats + audience segmentation based on buying intent of users Weekly & Monthly: Leads stats, Top questions asked by users, Useful Sales Intelligence Analytics",
+        source: "--",
+        type: "Text",
+        created: "9/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "in which company you have provided your chatbot We have provided our chatbot services to various companies across different industries.  These include: MultiBhashi, Selligion, drmalpani. com, indianangel. in May I know which company / industry are you coming from?",
+        source: "--",
+        type: "Text",
+        created: "15/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Can I cancel my subscription at any time?  You may cancel your subscription to BeyondChats at any time.  The cancellation will become effective at the end of your current billing cycle.  Upon cancellation, your service will remain active until the end of your paid billing term.",
+        source: "--",
+        type: "Text",
+        created: "8/6/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "What are the pricing plans of BeyondChats? We offer 3 different plans: Individual Plan at Rs 1,000 only, Startup Plan at Rs 8,000 only, and Enterprise Plan price based on your requirements. You can subscribe to the paid plans in a monthly or an annual billing cycle. [Know More](https://beyondchats.com/)",
+        source: "--",
+        type: "Text",
+        created: "20/2/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
+    {
+        data: "Contact Details Please write to contactbeyondchats@gmail.com and we'll get back to you real soon!",
+        source: "--",
+        type: "Text",
+        created: "13/3/2024",
+        actions: {
+            pencil: PencilLogo,
+            delete: DeleteLogo,
+        }
+    },
 ]
